1cdb8a0a2ac8c7af042834e198c01284ccd45f38
adiboy6 <aditya.reddy.97@gmail.com>
2020-03-17 18:21:32 +0530

    terraform experimenting

---
+++ terraform-practice/nic.tf
@@ -0,0 +1,12 @@
+# Create network interface
+resource "azurerm_network_interface" "nic" {
+  name                      = "myNIC"
+  location                  = azurerm_resource_group.rg.location
+  resource_group_name       = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "myNICConfg"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "dynamic"
+  }
+}
